cae05746994c094c133bd09e01a82d018b801dc4
Thayana Mamoré <thayana.mamore@topi.io>
2021-02-05 19:13:17 -0300

    Structuring UI

---
+++ src/components/ProfileImg/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ProfileImg = styled.img`
+    border-radius: 50%;
+    width: 100%;
+    max-width: 20rem;
+    height: auto;
+    padding: 2rem 1rem;
+`;
+
+export default ProfileImg;
