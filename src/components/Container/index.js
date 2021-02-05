21df2729986f86146c59932d9dafba0a70c1cf7b
Thayana Mamoré <thayana.mamore@topi.io>
2021-02-05 19:44:21 -0300

    update container style

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,9 +3,8 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 100%;
   max-width: 80vw;
-  //padding-top: 45px;
+  padding-top: 45px;
   margin: auto 10%;
-  background-color: white;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
