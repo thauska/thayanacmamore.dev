a969cc174df70f285288302d5fce4ea17f5f9aa9
Thayana Mamoré <thayana.mamore@topi.io>
2021-02-05 19:40:10 -0300

    update head

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -11,7 +11,7 @@ export default function Home({ title, description, bg }) {
       <meta property="og:locale" content="pt_BR" />
 
       <meta property="og:title" content={title} />
-      <meta property="og:site_name" content="aluraquiz" />
+      <meta property="og:site_name" content="resume" />
 
       <meta property="og:description" content={description} />
       <meta property="og:image" content={bg} />
